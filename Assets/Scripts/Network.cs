2385139f43e76bcd611fb0c8925c84e4b6508f47
Kit <chanwk3@gmail.com>
2014-08-23 22:01:11 +1200

    networked clean scene is go!

---
+++ Assets/Scripts/Network.cs
@@ -0,0 +1,38 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Network : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () 
+    {
+        PhotonNetwork.logLevel = PhotonLogLevel.Full;
+        PhotonNetwork.ConnectUsingSettings("0.1");
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+    void OnGUI()
+    {
+        GUILayout.Label(PhotonNetwork.connectionStateDetailed.ToString());
+    }
+
+    void OnJoinedLobby()
+    {
+        PhotonNetwork.JoinRandomRoom();
+
+    }
+
+    void OnJoinedRoom()
+    {
+        PhotonNetwork.Instantiate("GamePlayer", Vector3.zero, Quaternion.identity, 0);
+    }
+
+    void OnPhotonRandomJoinFailed()
+    {
+        PhotonNetwork.CreateRoom(null);
+    }
+}
