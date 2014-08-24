7ba2ea52fc09ecd027c3b2141bacccd6c01df3ba
Danny <duke1102@gmail.com>
2014-08-25 00:11:49 +0200

    Merge branch 'develop' of https://github.com/code-minion/whos-haunting-who into develop
    Conflicts:
    	ProjectSettings/TagManager.asset

---
+++ Assets/Interactable.cs
@@ -0,0 +1,40 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Interactable : MonoBehaviour 
+{
+    public enum Interaction
+    {
+        None,
+        Drag,
+        Hold,
+    }
+    public Interaction currentInteration = Interaction.None;
+    Transform target;
+    Vector3 previousPosOfTarget;
+    Vector3 offsetFromTarget;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () 
+    {
+        if (currentInteration == Interaction.Drag)
+        {
+            Vector3 diff = target.position - previousPosOfTarget;
+            if (diff.magnitude < 0.05) return;
+            transform.position = transform.position + diff;
+        }
+	}
+
+    [RPC]
+    void Grab(Transform _transform)
+    {
+        target = _transform;
+        previousPosOfTarget = target.position;
+        offsetFromTarget = transform.position - target.position;
+    }
+}
