f76a28ef0dceb9ed064290045cbbe170cd2c584b
Bonni Rambatan <bonni.rambatan@gmail.com>
2014-09-21 16:31:29 +0700

    Door can now be opened and closed -- but ProBuilder objects refuse to follow their parents! :O

---
+++ Assets/Scripts/DoorAnimator.cs
@@ -0,0 +1,42 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class DoorAnimator : MonoBehaviour {
+
+	Animator target;
+
+	bool doorActive;
+
+	// Use this for initialization
+	void Start () {
+		target = GetComponent<Animator>();
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (doorActive) {
+			if (Input.GetKeyDown (KeyCode.Space)) {
+				ActivateDoor ();
+			}
+		}
+	}
+
+	void ActivateDoor () {
+		bool doorOpen = target.GetBool ("Open");
+		target.SetBool ("Open", !doorOpen);
+		Debug.Log ("DOOR ACTIVATED");
+	}
+
+	void OnTriggerEnter (Collider other) {
+		if (other.CompareTag ("Player")) {
+			doorActive = true;
+		}
+	}
+
+	void OnTriggerExit (Collider other) {
+		if (other.CompareTag ("Player")) {
+			doorActive = false;
+		}
+	}
+}
