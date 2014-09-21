0c711121ac79256907d0cf62c27799ad6b92096d
Bonni Rambatan <bonni.rambatan@gmail.com>
2014-09-21 13:52:13 +0700

    Added scripts for flickering candles and light bulbs (thanks, Kit!)

---
+++ Assets/Scripts/flickerCandle.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class flickerCandle : MonoBehaviour {
+
+	Light target;
+
+	[Range(0,1)]public float minValue;
+	[Range(0,1)]public float maxValue;
+	float range;
+	public float speedScale = 3f;
+
+	// Use this for initialization
+	void Start () {
+		target = GetComponent<Light>();
+		range = (maxValue - minValue);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+		float waveValue = Mathf.PerlinNoise (Time.time * speedScale, 0f);
+
+		float value = waveValue * range;
+		value += minValue;
+		
+		target.intensity = value;
+	}
+}
---
+++ Assets/Scripts/flickerLightBulb.cs
@@ -0,0 +1,43 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class flickerLightBulb : MonoBehaviour {
+	
+	Light target;
+	
+	[Range(0,2)]public float minValue;
+	[Range(0,2)]public float maxValue;
+	float range;
+	public float speedScale = 7f;
+
+	bool flicker = true;
+
+	float nextTime;
+	float delay = Random.Range (3f,8f);
+
+	// Use this for initialization
+	void Start () {
+		target = GetComponent<Light>();
+		range = (maxValue - minValue);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+		float waveValue = Mathf.PerlinNoise (Time.time * speedScale, 0f);
+		
+		float value = waveValue * range;
+		value += minValue;
+
+		if (Time.time > nextTime) {
+			flicker = !flicker;
+			nextTime = Time.time + delay;
+		}
+
+		if (flicker == true) {
+						target.intensity = value;
+				} else {
+						target.intensity = 0.8f;
+				}
+	}
+}
